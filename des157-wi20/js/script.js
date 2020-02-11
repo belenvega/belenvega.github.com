9b21255af3b76bb01c0513aab26873bb830d2089
Belen Vega <blnvega@ucdavis.edu>
2020-02-11 10:51:15 -0800

    uploaded sketch

--- des157-wi20/js/script.js
+++ des157-wi20/js/script.js
@@ -1,17 +1,17 @@
 function setup() {
     var myCanvas = createCanvas(800, 250);
-    frameRate(8);
+    frameRate(10);
     noStroke();
-    background(random(255), random(255), random(255));
+    background(255);
     myCanvas.parent(mySketch);
   }
   
   function draw() {
-    if(mouseIsPressed) {
-      fill(0);
-    } else {
-      fill(random(255), random(255), random(255), random(255));
-    }
-    var circSize = random(200);
-    ellipse(mouseX, mouseY, circSize, circSize);
+    fill(255, 60);
+    rect(0, 0, width, height);
+
+    fill(0);
+    var circSize = random(50);
+    rect(mouseX, mouseY, circSize, circSize * 2);
+    /* $("rect").fadeOut("slow"); */
   }
